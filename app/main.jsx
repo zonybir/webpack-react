5311fd41587d7b74f6dd70ca55a7bedb3a8661c4
zony-com <zony_zhang@uyixing.com>
2015-11-18 11:57:50 +0800

    webpack react

---
+++ app/main.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+/*var Hello = require('./component.jsx');
+React.render(<Hello name="Nate" />,document.getElementById('app'));*/
+var TickTock = require ('./component.jsx');
+
+React.render(
+  <TickTock />,
+  document.getElementById('app')
+);
