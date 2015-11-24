becee568ea40eacc71cc79a0fea66bbc29cbf6a5
zony-com <zony_zhang@uyixing.com>
2015-11-24 10:34:25 +0800

    *.*

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ gulp.task('webpack',function(){
 	webpack({
 		//configuration
 		entry:[path.resolve(__dirname,'react/main.js')],
+		output:{
+			path:path.resolve(__dirname,'gulp_webpack_react/build'),
+			filename:'build.js'
+			},
 		module: {
 		  	 loaders: [{
 		     		test: /\.js[x]?$/, // 用正则来匹配文件路径，这段意思是匹配 js 或者 jsx
@@ -17,10 +21,7 @@ gulp.task('webpack',function(){
 		if (err) throw new gutil.PluginError('webpack',err);
 		gutil.log(['webpack'],stats.toString({
 			//output configuration
-			output:{
-				path:path.resolve(__dirname,'gulp_webpack_react/build'),
-				filename:'build22.js'
-			}
+			
 		}))
 	})
 })
